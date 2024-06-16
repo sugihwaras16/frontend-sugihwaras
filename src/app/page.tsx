3d4af4b4833c2ae84644aa3da6325baaba4f4411
IqbalFebrianwar <zonelyrics26@gmail.com>
2024-06-17 02:00:21 +0700

    feat : add navbar

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import NavbarComponents from "./components/navbar";
+
+export default function Home() {
+  return (
+    <main className="w-full justify-center flex px-20">
+      <NavbarComponents />
+    </main>
+  );
+}
