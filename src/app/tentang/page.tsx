c5322eed41a3bcdf8e16cc046a5e35af50cd46fd
IqbalFebrianwar <zonelyrics26@gmail.com>
2024-06-17 19:01:21 +0700

    Feat : Perubahan Warna 2

--- src/app/tentang/page.tsx
+++ src/app/tentang/page.tsx
@@ -5,7 +5,7 @@ export default function Tentang() {
   return (
     <main className="w-full flex flex-col items-center">
       <div className="w-full flex justify-start px-5 md:px-32 lg:px-48 py-3">
-        <h1 className="text-black uppercase font-semibold">Tentang Kami</h1>
+        <h1 className="text-white uppercase font-semibold">Tentang Kami</h1>
       </div>
     </main>
   );
